07d40928853a38fab6cf74bc3ef140dbd9c2b687
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-29 02:47:03 -0800

    Working but dosent look pretty

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,47 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <v-toolbar class="transparent">
+    <v-toolbar-title>A Work In Progress</v-toolbar-title>
+  </v-toolbar>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">12</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">6</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 3" :key="`4${i}`" xs4>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">4</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 6" :key="`2${i}`" xs2>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">2</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BlogHome',
-  props: {
-    msg: String
-  }
+  name: 'BlogHome'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -54,5 +61,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <BlogHome msg="Welcome to Your Vue.js App"/>
-  </div>
+  <BlogHome />
 </template>
 
 <script>
