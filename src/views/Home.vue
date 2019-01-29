8dc69628b1f55d2e651497c2764545d2a38189f5
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-29 02:00:12 -0800

    Working Router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-
+    <BlogHome msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import BlogHome from '@/components/BlogHome.vue'
 
 export default {
-  name: 'home'
-  // components: {
-  //   HelloWorld
-  // }
+  name: 'home',
+  components: {
+    BlogHome
+  }
 }
 </script>
