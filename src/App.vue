34cf392cddc0628376061dc5352f4add5e4376ac
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-31 13:14:40 -0800

    Working Hover effect

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,88 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-hover>
+  <v-card
+    slot-scope="{ hover }"
+    class="mx-auto"
+    color="grey lighten-4"
+    max-width="600"
+  >
+    <v-img
+      :aspect-ratio="16/9"
+      src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+    >
+      <v-expand-transition>
+        <div
+          v-if="hover"
+          class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+          style="height: 100%;"
+        >
+          $14.99
+        </div>
+      </v-expand-transition>
+    </v-img>
+    <v-card-text
+      class="pt-4"
+      style="position: relative;"
+    >
+      <v-btn
+        absolute
+        color="orange"
+        class="white--text"
+        fab
+        large
+        right
+        top
+      >
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+      <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
+      <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+      <div class="font-weight-light title mb-2">
+        Our Vintage kitchen utensils delight any chef.<br>
+        Made of bamboo by hand
+      </div>
+    </v-card-text>
+  </v-card>
+</v-hover>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.v-card--reveal {
+align-items: center;
+bottom: 0;
+justify-content: center;
+opacity: .5;
+position: absolute;
+width: 100%;
 }
 </style>
