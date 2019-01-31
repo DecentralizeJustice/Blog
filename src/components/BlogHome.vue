a160a2ae350d41ae3ea2b5ce6c5946d13804e2f7
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-30 18:10:57 -0800

    Test

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -23,7 +23,7 @@
             class="d-flex transition-slow-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
             style="height: 100%;"
           >
-          {{ post.title }}
+          "lets try"
           </div>
         </v-expand-transition>
       </v-img>
