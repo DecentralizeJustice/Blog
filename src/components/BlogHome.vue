4448047411a818b938a0cb425000c352bc7e6307
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-31 18:51:29 -0800

    Workinf after adding space

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-toolbar>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-toolbar-title>Multisig Crypto</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
   <!-- <a href="https://www.w3schools.com"> -->
@@ -14,6 +14,7 @@
          max-width="600"
        >
          <v-img
+          v-if="post != undefined"
            :aspect-ratio="16/9"
            :src= 'post.coverPhoto.url'
          >
@@ -23,31 +24,16 @@
                class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
                style="height: 100%;"
              >
-               $14.99
+              {{ post.title }}
              </div>
            </v-expand-transition>
          </v-img>
          <v-card-text
-           class="pt-4"
+           class="pt-4 hidden-md-and-up"
            style="position: relative;"
          >
-           <v-btn
-             absolute
-             color="orange"
-             class="white--text"
-             fab
-             large
-             right
-             top
-           >
-             <v-icon>mdi-cart</v-icon>
-           </v-btn>
-           <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
-           <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
-           <div class="font-weight-light title mb-2">
-             Our Vintage kitchen utensils delight any chef.<br>
-             Made of bamboo by hand
-           </div>
+         <h3 class="display-1 font-weight-light orange--text mb-2 " v-if="post != undefined">{{ post.title }}</h3>
+
          </v-card-text>
        </v-card>
      </v-hover>
