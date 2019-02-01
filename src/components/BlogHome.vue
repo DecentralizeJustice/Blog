eb30d329659efb3bfe08c6de2ba940af5bcb5e87
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-31 16:26:23 -0800

    Working Now?

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,68 +1,66 @@
 <template>
-  <div>
-  <v-toolbar>
-    <v-toolbar-title>Multisig Crypto</v-toolbar-title>
-  </v-toolbar>
-  <!-- <a href="https://www.w3schools.com"> -->
-  <v-hover>
-    <v-card
-      slot-scope="{ hover }"
-      class="mx-auto"
-      color="grey lighten-4"
-      max-width="600"
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-hover>
+  <v-card
+    slot-scope="{ hover }"
+    class="mx-auto"
+    color="grey lighten-4"
+    max-width="600"
+  >
+    <v-img
+      :aspect-ratio="16/9"
+      src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+    >
+      <v-expand-transition>
+        <div
+          v-if="hover"
+          class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+          style="height: 100%;"
+        >
+          $14.99
+        </div>
+      </v-expand-transition>
+    </v-img>
+    <v-card-text
+      class="pt-4"
+      style="position: relative;"
     >
 
-      <v-img
-        v-if="post != undefined"
-        :aspect-ratio="16/9"
-        :src= 'post.coverPhoto.url'
-      >
-        <v-expand-transition>
-          <div
-            v-if="hover"
-            class="d-flex transition-slow-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
-            style="height: 100%;"
-          >
-          {{ post.title }}
-          </div>
-        </v-expand-transition>
-      </v-img>
-      <v-card-text
-          class="pt-4 hidden-md-and-up"
-          style="position: relative;"
-        >
-          <h3 class="display-1 font-weight-light orange--text mb-2" v-if="post == undefined">Test</h3>
-          <h3 class="display-1 font-weight-light orange--text mb-2" v-else>{{ post.title }}</h3>
-        </v-card-text>
-    </v-card>
-  </v-hover>
-  <!-- </a> -->
-  </div>
+      <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
+      <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+      <div class="font-weight-light title mb-2">
+        Our Vintage kitchen utensils delight any chef.<br>
+        Made of bamboo by hand
+      </div>
+    </v-card-text>
+  </v-card>
+</v-hover>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import gql from 'graphql-tag'
-export default {
-  name: 'BlogHome',
 
-  apollo: {
-    post: gql`{
-      post(where: {id: "cjrg2t0z7b9u30a23puhi9gxk"}) {
-         coverPhoto {
-           url
-         }
-         title
-      }
-    }`
-  },
-  data () {
-    return {
-      tests: ''
-    }
+
+  export default {
+    name: 'BlogHome',
   }
-}
 </script>
-
 <style>
 .v-card--reveal {
 align-items: center;
