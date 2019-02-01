447d455e8b7d689caf74a5696eb2fd710167940b
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-31 17:38:20 -0800

    Add Toolbar, now working

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,63 +1,88 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Multisig Crypto</span>
-      </v-toolbar-title>
+  <div>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-    <v-content>
-      <v-hover>
-  <v-card
-    slot-scope="{ hover }"
-    class="mx-auto"
-    color="grey lighten-4"
-    max-width="600"
-  >
-    <v-img
-      :aspect-ratio="1.6180/1"
-      src="https://images.pexels.com/photos/1430931/pexels-photo-1430931.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-    >
-      <v-expand-transition>
-        <div
-          v-if="hover"
-          class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
-          style="height: 100%;"
-        >
-          1
-        </div>
-      </v-expand-transition>
-    </v-img>
-    <v-card-text
-      class="pt-4"
-      style="position: relative;"
-    >
-
-      <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
-      <h3 class="display-1 font-weight-light orange--text mb-2">oking utensils</h3>
-      <div class="font-weight-light title mb-2">
-        ccccef.<br>
-
-      </div>
-    </v-card-text>
-  </v-card>
-</v-hover>
-    </v-content>
-  </v-app>
+  <!-- <a href="https://www.w3schools.com"> -->
+  <v-hover>
+       <v-card
+         slot-scope="{ hover }"
+         class="mx-auto"
+         color="grey lighten-4"
+         max-width="600"
+       >
+         <v-img
+           :aspect-ratio="16/9"
+           src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+         >
+           <v-expand-transition>
+             <div
+               v-if="hover"
+               class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+               style="height: 100%;"
+             >
+               $14.99
+             </div>
+           </v-expand-transition>
+         </v-img>
+         <v-card-text
+           class="pt-4"
+           style="position: relative;"
+         >
+           <v-btn
+             absolute
+             color="orange"
+             class="white--text"
+             fab
+             large
+             right
+             top
+           >
+             <v-icon>mdi-cart</v-icon>
+           </v-btn>
+           <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
+           <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+           <div class="font-weight-light title mb-2">
+             Our Vintage kitchen utensils delight any chef.<br>
+             Made of bamboo by hand
+           </div>
+         </v-card-text>
+       </v-card>
+     </v-hover>
+  <!-- </a> -->
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'BlogHome'
+  name: 'BlogHome',
+  data () {
+    return {
+      tests: ''
+    }
+  }
 }
 </script>
-<style>
+
+<style scoped>
 .v-card--reveal {
 align-items: center;
 bottom: 0;
 justify-content: center;
-opacity: .5;
+opacity: .9;
 position: absolute;
 width: 100%;
+}
+a {
+  color: none;
+  text-decoration: none; /* no underline */
 }
 </style>
