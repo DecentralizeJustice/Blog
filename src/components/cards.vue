477bd931371ad1451b03850656bbe8796d9aa10c
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-04 21:55:39 -0800

    More Control of spacing coming

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,16 +1,13 @@
 <template>
 
   <!-- <a href="https://www.w3schools.com"> -->
-  <v-container grid-list-md text-xs-center v-if="posts != undefined">
-  <v-layout row wrap>
 
+  <v-layout align-start row fill-height v-if="posts != undefined">
     <v-flex v-for="post in posts" :key="post.id" xs4>
       <v-hover>
            <v-card
              slot-scope="{ hover }"
              class="mx-auto"
-             color="grey lighten-4"
-
            >
            <v-img
              :aspect-ratio="1/1.5"
@@ -19,7 +16,7 @@
              <v-expand-transition >
                <div
                  v-if="hover"
-                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text hidden-md-and-down"
+                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text hidden-sm-and-down"
                  style="height: 100%;"
                >
                 {{ post.title }}
@@ -35,9 +32,7 @@
            </v-card>
          </v-hover>
     </v-flex>
-
   </v-layout>
-</v-container>
 
 </template>
 
