6fa1a7fcc58ec4846652a2c9af011cb37218a2e0
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-05 10:58:40 -0800

    Added links

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <!-- <a href="https://www.w3schools.com"> -->
-
   <v-layout align-start row fill-height v-if="posts != undefined">
-    <v-flex v-for="post in posts" :key="post.id" xs3 offset-xs1 class="mt-5 pt-1">
+    <v-flex v-for="post in posts" :key="post.id"
+      md3 offset-md1 class="mt-5 pt-1">
+      <a href="https://www.google.com">
       <v-hover>
            <v-card
              slot-scope="{ hover }"
@@ -16,7 +16,9 @@
              <v-expand-transition >
                <div
                  v-if="hover"
-                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text hidden-sm-and-down text-xs-center"
+                 class="d-flex transition-fast-in-fast-out orange darken-2
+                 v-card--reveal display-3 white--text
+                 hidden-sm-and-down text-xs-center"
                  style="height: 100%;"
                >
                 {{ post.title }}
@@ -27,10 +29,13 @@
              class="pt-4 hidden-md-and-up"
              style="position: relative;"
            >
-             <h3 class="display-1 font-weight-light orange--text mb-2 " >{{ post.title }}</h3>
+             <h3 class="display-1 font-weight-light orange--text mb-2 " >
+               {{ post.title }}
+             </h3>
           </v-card-text>
            </v-card>
          </v-hover>
+         </a>
     </v-flex>
   </v-layout>
 
