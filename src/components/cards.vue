560e2494b6230e18bec63d940df50f268c02135d
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-04 21:34:28 -0800

    Working Aspect Ratio

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,43 +1,44 @@
 <template>
-  <div>
 
   <!-- <a href="https://www.w3schools.com"> -->
   <v-container grid-list-md text-xs-center v-if="posts != undefined">
+  <v-layout row wrap>
 
-  <v-hover v-for="post in posts" :key="post.id">
-       <v-card
-         slot-scope="{ hover }"
-         class="mx-auto"
-         color="grey lighten-4"
-         max-width="600"
-       >
-       <v-img
-         :aspect-ratio="16/9"
-         :src= 'post.coverPhoto.url'
-       >
-         <v-expand-transition>
-           <div
-             v-if="hover"
-             class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
-             style="height: 100%;"
+    <v-flex v-for="post in posts" :key="post.id" xs4>
+      <v-hover>
+           <v-card
+             slot-scope="{ hover }"
+             class="mx-auto"
+             color="grey lighten-4"
+
            >
-            {{ post.title }}
-           </div>
-         </v-expand-transition>
-        </v-img>
-      <v-card-text
-         class="pt-4 hidden-md-and-up"
-         style="position: relative;"
-       >
-         <h3 class="display-1 font-weight-light orange--text mb-2 " >{{ post.title }}</h3>
+           <v-img
+             :aspect-ratio="1/1.5"
+             :src= 'post.coverPhoto.url'
+           >
+             <v-expand-transition >
+               <div
+                 v-if="hover"
+                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text hidden-md-and-down"
+                 style="height: 100%;"
+               >
+                {{ post.title }}
+               </div>
+             </v-expand-transition>
+            </v-img>
+          <v-card-text
+             class="pt-4 hidden-md-and-up"
+             style="position: relative;"
+           >
+             <h3 class="display-1 font-weight-light orange--text mb-2 " >{{ post.title }}</h3>
+          </v-card-text>
+           </v-card>
+         </v-hover>
+    </v-flex>
 
-      </v-card-text>
-       </v-card>
-     </v-hover>
+  </v-layout>
+</v-container>
 
-  </v-container>
-  <!-- </a> -->
-  </div>
 </template>
 
 <script>
