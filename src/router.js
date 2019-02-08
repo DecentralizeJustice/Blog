3aecec34e6baae31f1705fa3695df21bf01dbc85
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-08 12:38:08 -0800

    try and fix

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import embraceDifference from './components/embraceDifference/main.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     { path: '/', component: Home },
