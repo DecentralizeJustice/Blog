ff5075a95a08758178cbaa39fe448c7db83be17b
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-10 21:23:09 -0800

    Added lazy loading to post

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import embraceWeirdness from './components/embraceWeirdness/main.vue'
+const embraceWeirdness = () => import('./components/embraceWeirdness/main.vue')
+
 Vue.use(Router)
 
 export default new Router({
