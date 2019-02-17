86b3ab176f500880750b6deccae5c6bfd1403fb5
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-17 12:29:34 -0800

    Test second section

--- src/components/embraceWeirdness/main.vue
+++ src/components/embraceWeirdness/main.vue
@@ -56,29 +56,64 @@
            alt="Scary Driver" style="top:10vh;width:100vw;height:auto;left:0vw;position:relative;overflow:hidden;"/>
         </v-flex>
 
-        <!-- <v-flex xs12 class="hidden-sm-and-down"  style="position:relative;height:101vh;background-color: #486E70;">
-          <p style="position:relative;top: 10vh;width:61.8%;color: white;text-align:left;left:8%;"
-             class="font-weight-black display-3" >
-            Would you take a ride from a driver who learned how to drive from studying and riding bicycles?
-          </p>
-          <video
-          muted loop autoplay
-          data-aos="fade-up"
-          src="https://media.giphy.com/media/l0MYQ2rekCnAE0CQg/giphy.mp4"
-          alt="Scary Driver" style="top:12vh;position:relative;left:12vw;height:auto;width:40vw;"/>
+        <v-flex xs12 class="hidden-sm-and-down"  style="position:relative;height:100%;background-color: #486E70;">
+          <section
+          style="padding:2vw;position;relative;margin-top:2.5%;margin-bottom:2.5%;width:50vw;margin-left:5vw;
+          font-size:1.4rem;line-height:1.5rem;background-color:white;
+          border-style:solid;border-color: rgb(29, 66, 76);border-width: 1vh;" >
+          <p><span class="firstcharacter">No!</span>
+          would  hopefully be your answer. You should say no because your driver
+          probably isn't good at drvivng.This is because they made a connections and skills based on similarities that don’t really matter.
+           In this example it is comically clear that this driver does
+          not understand how to drive well. Cars and bikes share some similarities, but cars were an innovative technology compared to bikes,
+          so drawing similarities between the two technologies isn’t really useful and can be dangerous. When I made this mistake,
+        but with cryptocurrencies, it wasn't as comical or dangerous though.</p>
+
+          When I first started learning about cryptocurrencies I wanted to understand everything about them. I wanted to know what types there were,
+           how they worked, how I could get involved, and most importantly, what they would look like in the future. In order to learn about the
+           future of  cryptocurrencies, I decided to look to the past for guidance and since the internet and cryptocurrencies seemed similar I decided
+            to study the history of the internet in order to learn more about how cryptocurrencies might grow and evolve in the future. At the time
+            this made perfect sense to me, since we are taught that history is a great tool for learning about the future. This didn’t really help me
+            learn anything about cryptocurrencies, although the history of the internet is a great thing to know about. My mistake was years ago, and
+            I know why my plan to predict the
+            future of crypto didn't work; Drawing similarities between innovative technologies doesn't work because of the weird nature humanity and innovation.
+
+          </section>
+          <img     data-aos="fade-left"
+          src="http://www.mypressplus.com/wp-content/uploads/2018/02/history.jpg"
+          alt="Studying"
+          style="top:8%;position:absolute;right:8vw;height:auto;width:28vw;"
+          >
+          <img     data-aos="fade-left"
+          src="https://images.pexels.com/photos/545062/pexels-photo-545062.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="Studying 2"
+          style="top:54%;position:absolute;right:8vw;height:auto;width:28vw;"
+          >
         </v-flex>
-        <v-flex xs12 class="hidden-md-and-up" style="position:relative;height:90vh;background-color: rgb(29, 66, 76);">
-           <v-flex   class="justify-center" style="margin: auto; width:90%;">
-             <p  style="position:relative;top:10vh;color:white;text-align:center;" class="display-1">
-               Would you take a ride from a driver who learned how to drive from studying and riding bicycles?
-             </p>
-           </v-flex>
-           <video
-           muted loop autoplay
-           data-aos="fade-up"
-           src="https://media.giphy.com/media/l0MYQ2rekCnAE0CQg/giphy.mp4"
-           alt="Scary Driver" style="top:10vh;width:50vh;height:auto;left:0vw;position:relative;overflow:hidden;"/>
-        </v-flex> -->
+        <v-flex xs12 class="hidden-md-and-up" style="position:relative;height:100%;background-color: #486E70;">
+          <section
+          style="padding:2vw;position;relative;margin-top:2%;margin-bottom:2%;width:95vw;margin-left:2.5vw;
+          font-size:1.2rem;line-height:1.5rem;background-color:white;
+          border-style:solid;border-color: rgb(29, 66, 76);border-width: 1vh;" >
+          <p><span class="firstcharacter">No!</span>
+          would  hopefully be your answer. You should say no because your driver
+          probably isn't good at drvivng.This is because they made a connections and skills based on similarities that don’t really matter.
+           In this example it is comically clear that this driver does
+          not understand how to drive well. Cars and bikes share some similarities, but cars were an innovative technology compared to bikes,
+          so drawing similarities between the two technologies isn’t really useful and can be dangerous. When I made this mistake,
+        but with cryptocurrencies, it wasn't as comical or dangerous though.</p>
+
+          When I first started learning about cryptocurrencies I wanted to understand everything about them. I wanted to know what types there were,
+           how they worked, how I could get involved, and most importantly, what they would look like in the future. In order to learn about the
+           future of  cryptocurrencies, I decided to look to the past for guidance and since the internet and cryptocurrencies seemed similar I decided
+            to study the history of the internet in order to learn more about how cryptocurrencies might grow and evolve in the future. At the time
+            this made perfect sense to me, since we are taught that history is a great tool for learning about the future. This didn’t really help me
+            learn anything about cryptocurrencies, although the history of the internet is a great thing to know about. My mistake was years ago, and
+            I know why my plan to predict the
+            future of crypto didn't work; Drawing similarities between innovative technologies doesn't work because of the weird nature humanity and innovation.
+
+          </section>
+        </v-flex>
 
   <!-- <section
    style="background-color: #486E70;height: 89vh;position: relative;padding-top:5vh;">
