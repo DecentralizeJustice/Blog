7382437ac70b77f35f54b08461547e6e8e8edd20
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-17 16:55:46 -0800

    Image test

--- src/components/embraceWeirdness/main.vue
+++ src/components/embraceWeirdness/main.vue
@@ -198,6 +198,72 @@
           </section>
         </v-flex>
 
+        <v-flex xs12 class="hidden-sm-and-down"  style="position:relative;height:100vh;background-color: rgb(29, 66, 76);">
+          <section
+          style="position:relative;width:50vw;top:4vh;left:5vw;font-size:1.5rem;line-height: 1.5rem;background-color:white;
+          border-style:solid;border-color:#486E70; border-width:1vh;padding:2vw" >
+          <p>We should focus on how weird bitcoin and ethereum is compared to old tech and how
+            they will be different in the future, when compared to old tech. It is almost impossible to try and predict the specifics
+            of future technologies, but we can imagine what they might look like. The process of imagining instead of
+            predicting the future, unburdens us from having to be right and looking to history to support our statements.
+            Imagining instead allows us to explore infinite possibilites, granting us the complete freedom to dream. These dreams
+            may be weird and unfamiliar, and they should be.
+          We should embrace the weirdness of these new ideas and allow them to expand our understanding of the future and
+          ourselves, instead of trying to fit cryptocurrencies into our current
+          understanding of the world. We owe cryptocurrencies and ourselves the freedom to imagine.</p>
+
+          I want us to have more weird and imaginative conversations and less conversations focusing on the relatively mundane advancements.
+          There are a million articles about how cryptocurrencies will
+          make moving money faster, but very few on how decentralized organizations will change what it means to be human or how a
+          microtransaction focused economy could be a more equitable
+           way to distribute value. These types of conversations exist in certain communties and contexts,
+            such as futurist writings and queer theory, but I hope that we can have more imaginative and unbound
+            conversations everday.  We should let go of the past and  stop trying to predict the future.
+            We need to dream and imagine bigger things because the only
+            thing that truly limits our future is our collective
+            imagination about what the future could be.
+          </section>
+          <img     data-aos="fade-left"
+          src="https://media1.tenor.com/images/88d89612db5eaacc6c9c6ac6bf6cd6e7/tenor.gif?itemid=7715402"
+          alt="Studying 2"
+          style="top:25vh;position:absolute;left:60vw;height:auto;width:34vw;
+            border-style:solid;border-color: #486E70;border-width: 1vh;"
+          >
+        </v-flex>
+        <v-flex xs12 class="hidden-md-and-up" style="position:relative;height:100%;background-color:rgb(29, 66, 76); ">
+          <section
+          style="padding:2vw;position;relative;margin-top:2%;margin-bottom:2%;width:95vw;margin-left:2.5vw;
+          font-size:1.2rem;line-height:1.5rem;background-color:white;
+          border-style:solid;border-color:#486E70; border-width: 1vh;" >
+          <p> We should focus on how weird bitcoin and ethereum is compared to old tech and how
+              they will be different in the future, when compared to old tech. It is almost impossible to try and predict the specifics
+              of future technologies, but we can imagine what they might look like. The process of imagining instead of
+              predicting the future, unburdens us from having to be right and looking to history to support our statements.
+              Imagining instead allows us to explore infinite possibilites, granting us the complete freedom to dream. These dreams
+              may be weird and unfamiliar, and they should be.
+            We should embrace the weirdness of these new ideas and allow them to expand our understanding of the future and
+            ourselves, instead of trying to fit cryptocurrencies into our current
+            understanding of the world. We owe cryptocurrencies and ourselves the freedom to imagine.</p>
+
+            I want us to have more weird and imaginative conversations and less conversations focusing on the relatively mundane advancements.
+            There are a million articles about how cryptocurrencies will
+            make moving money faster, but very few on how decentralized organizations will change what it means to be human or how a
+            microtransaction focused economy could be a more equitable
+             way to distribute value. These types of conversations exist in certain communties and contexts,
+              such as futurist writings and queer theory, but I hope that we can have more imaginative and unbound
+              conversations everday.  We should let go of the past and  stop trying to predict the future.
+              We need to dream and imagine bigger things because the only
+              thing that truly limits our future is our collective
+              imagination about what the future could be.
+          </section>
+          <img     data-aos="fade-up"
+          src="https://media1.tenor.com/images/88d89612db5eaacc6c9c6ac6bf6cd6e7/tenor.gif?itemid=7715402"
+          alt="Studying 2"
+          style="top:50%;position:relative;left:0vw;height:auto;width:100vw;
+            "
+          >
+        </v-flex>
+
   <!--
 
   <section
