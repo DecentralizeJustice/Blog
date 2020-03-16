e0f13ce14324646143700be57854b4889816f1d6
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-03-16 14:57:21 -0400

    Text hot fix

--- src/components/knowyourlimits/7thParagraph.js
+++ src/components/knowyourlimits/7thParagraph.js
@@ -1,6 +1,6 @@
 const paragraph5 =
 `<p>These different levels of crypto are reasonable limits that help you keep all of your crypto. 
-Below is a tool that gives you a bunch of info about each tier and what tools to use. 
+Below is a tool that gives you info about each tier and what tools to use. 
 </p>
 
 Power To The People,<br>
