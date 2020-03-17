f4705d6454fc721b47fcd6fd11b98fe2faa1c285
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-03-16 22:43:15 -0400

    Copy hotfix

--- src/components/knowyourlimits/7thParagraph.js
+++ src/components/knowyourlimits/7thParagraph.js
@@ -1,6 +1,6 @@
 const paragraph5 =
 `<p>These different levels of crypto are reasonable limits that help you keep all of your crypto. 
-Below is a tool that gives you info about each tier and what tools to use. 
+Below is a tool that gives you info about each tier and what tools to use. Just type in the amount of crypto that you want to store and the tool will tell you how you should store your crypto. I will keep this info updated, and if you have any questions please reach out to me on twitter. 
 </p>
 
 Power To The People,<br>
